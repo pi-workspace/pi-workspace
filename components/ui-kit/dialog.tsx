85061f3c6d6853736f131d6c4008f032dc08481d
Ozzy <7268075+OfficialOzzy@users.noreply.github.com>
2026-07-21 19:29:13 +0100

    fix: remove translucent background on dialogs.

--- components/ui-kit/dialog.tsx
+++ components/ui-kit/dialog.tsx
@@ -38,7 +38,7 @@ export function Dialog({
             className={clsx(
               className,
               sizes[size],
-              'row-start-2 w-full min-w-0 rounded-t-3xl bg-control-background p-(--gutter) shadow-lg ring-1 ring-content-border [--gutter:--spacing(8)] sm:mb-auto sm:rounded-2xl forced-colors:outline',
+              'row-start-2 w-full min-w-0 rounded-t-3xl bg-content-background p-(--gutter) shadow-lg ring-1 ring-content-border [--gutter:--spacing(8)] sm:mb-auto sm:rounded-2xl forced-colors:outline',
               'transition duration-100 will-change-transform data-closed:translate-y-12 data-closed:opacity-0 data-enter:ease-out data-leave:ease-in sm:data-closed:translate-y-0 sm:data-closed:data-enter:scale-95'
             )}
           >
